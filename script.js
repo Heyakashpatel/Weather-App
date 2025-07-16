fa6278f39ac1955788f7425ee5d51a651a3fbd60
Heyakashpatel <153529483+Heyakashpatel@users.noreply.github.com>
2025-07-16 16:37:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const temp = document.querySelector('.temp');
 const wind = document.querySelector('.wind');
 const humid = document.querySelector('.humid');
 const desc = document.querySelector('.desc');
+const loc_name = document.querySelector('.loc-name');
 const weather_img = document.querySelector('.weath-img');
 
 
@@ -48,6 +49,10 @@ async function weatherInfo(value){
 }
 
 
+window.addEventListener('DOMContentLoaded', () => {        
+  weatherInfo("India");          
+});
+
 
 
 // Dark Mode
@@ -64,6 +69,7 @@ const checkbox = document.getElementById('toggle-checkbox');
     temp_info_main.classList.toggle('active', this.checked);
     additional_info.classList.toggle('active', this.checked);
     input_info.classList.toggle('active', this.checked);
+      loc_name.classList.toggle('active', this.checked);
   });
 
 
